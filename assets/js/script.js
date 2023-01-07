e96e8f69e0cdf16b3e00b244255f69854bd3102f
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-01-06 16:12:13 -0800

    add functionality to save input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,6 +9,26 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  $(".saveBtn").on("click", saveInput);
+
+  function saveInput(event) {
+    //prevent the default behavior of the button
+    event.preventDefault();
+
+    //store the parent div of the clicked buttn with an id in a variable for later use
+    let parentDiv;
+    if (event.target.matches("i")) {
+      parentDiv = $(event.target).parents().eq(1);
+    } else {
+      parentDiv = $(event.target).parent();
+    }
+    
+    //save the text value in local storage
+    console.log($(parentDiv).attr("id") + ", " + $(parentDiv).children().eq(1).val());
+    localStorage.setItem($(parentDiv).attr("id"), $(parentDiv).children().eq(1).val());
+    
+  }
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
